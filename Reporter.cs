087b0bd0ed87cbf681bc893a7813802d5589baf7
shweta-shinde <47037519+shweta-shinde@users.noreply.github.com>
2020-08-29 19:55:28 +0530

    Plug-in of different reporters

---
+++ Reporter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+interface IReporter
+{
+    void report(float bpm, float spo2, float respRate);
+}
+class Reporter1 : IReporter
+{
+    public void report(float bpm,float spo2,float respRate)
+    {
+        Console.WriteLine("Reporter-1");
+        bool status = Checker.vitalsAreOk(bpm, spo2, respRate);
+        if (status)
+            Console.WriteLine("All Vitals OK");
+    }
+}
+class Reporter2 : IReporter
+{
+    public void report(float bpm, float spo2, float respRate)
+    {
+        Console.WriteLine("Reporter-2");
+        bool status = Checker.vitalsAreOk(bpm, spo2, respRate);
+        if (status)
+            Console.WriteLine("All Vitals OK");
+    }
+}
