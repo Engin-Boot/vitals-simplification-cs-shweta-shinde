b79c9f12e82e6474b1165da0e74dc104885a74df
shweta-shinde <47037519+shweta-shinde@users.noreply.github.com>
2020-08-29 13:53:18 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -3,32 +3,21 @@ using System.Diagnostics;
 
 class Checker
 {
-    static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
-            return false;
-        }
-        return true;
+    public static bool vitalsAreOk(float bpm, float spo2, float respRate)
+    {
+        return checkBpm(bpm) && checkSpo2(spo2) && checkRespRate(respRate);
+        
     }
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
-        }
+    static bool checkBpm(float bpm)
+    {
+        return (bpm >= 70 && bpm <= 150);
     }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
-        }
+    static bool checkSpo2(float spo2)
+    {
+        return (spo2 >= 90);
     }
-    static int Main() {
-        ExpectTrue(vitalsAreOk(100, 95, 60));
-        ExpectFalse(vitalsAreOk(40, 91, 92));
-        Console.WriteLine("All ok");
-        return 0;
+    static bool checkRespRate(float respRate)
+    {
+        return (respRate >= 30 && respRate <= 95);
     }
 }
