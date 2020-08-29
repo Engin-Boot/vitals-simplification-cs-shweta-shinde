40b9fef7d1a542599006025bcf7cdc8bda21e64c
shweta-shinde <47037519+shweta-shinde@users.noreply.github.com>
2020-08-29 19:56:15 +0530

    Reporter plug-in added

--- checker.cs
+++ checker.cs
@@ -6,21 +6,51 @@ class Checker
     public static bool vitalsAreOk(float bpm, float spo2, float respRate)
     {
         return checkBpm(bpm) && checkSpo2(spo2) && checkRespRate(respRate);
-        
+
     }
     static bool checkBpm(float bpm)
     {
-        return (bpm >= 70 && bpm <= 150);
+        bool low = bpm < 70;
+        bool high = bpm > 150;
+        if(low)
+        {
+            Console.WriteLine("BPM is LOW {0}",bpm);
+            return false;
+        }
+        if(high)
+        {
+            Console.WriteLine("BPM is HIGH {0}",bpm);
+            return false;
+        }
+        return true;
     }
     static bool checkSpo2(float spo2)
     {
-        return (spo2 >= 90);
+        bool low= (spo2 < 90);
+        if(low)
+        {
+            Console.WriteLine("SPO2 is LOW {0}",spo2);
+        }
+        return true;
     }
     static bool checkRespRate(float respRate)
     {
-        return (respRate >= 30 && respRate <= 95);
+        bool low = (respRate < 30);
+        bool high = (respRate > 95);
+        if (low)
+        {
+            Console.WriteLine("RESP_RATE is LOW {0}", respRate);
+            return false;
+        }
+        if (high)
+        {
+            Console.WriteLine("RESP_RATE is HIGH {0}", respRate);
+            return false;
+        }
+        return true;
+
     }
-     static void ExpectTrue(bool expression)
+    static void ExpectTrue(bool expression)
     {
         if (!expression)
         {
@@ -38,10 +68,16 @@ class Checker
     }
     public static int Main(string[] args)
     {
-        
-        ExpectTrue(vitalsAreOk(100, 95, 60));
+        IReporter reporter;
+        reporter = new Reporter1();
+        reporter.report(100, 95, 60);
+        reporter.report(40, 91, 92);
+        reporter = new Reporter2();
+        reporter.report(100, 95, 60);
+        reporter.report(40, 91, 92);
+        /*ExpectTrue(vitalsAreOk(100, 95, 60));
         ExpectFalse(vitalsAreOk(40, 91, 92));
-        Console.WriteLine("All ok");
+        Console.WriteLine("All ok");*/
         return 0;
     }
 }
